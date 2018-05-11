a6b07f60462fa92627769763ae5c86e03ad4bb6c
Sindri Guðmundsson <sindrig@tempo.io>
2018-05-11 11:28:41 +0000

    ok
    Former-commit-id: 4ed412941455a0f9c56594b9c75e89a36ba97e2c

--- infra/letsencrypt/build.sh
+++ infra/letsencrypt/build.sh
@@ -4,5 +4,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
     cd $DIR
 
     pip install -r requirements.txt -t lib
-    zip -qyr code.zip $DIR -x@.lambdaignore
+    zip -qyr code.zip . -x@.lambdaignore
 )
