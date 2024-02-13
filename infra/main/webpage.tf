7c9b8a5ae46cf1fa530d0e04941271b0809b2d8d
Sindri Guðmundsson <sindrigudmundsson@gmail.com>
2024-02-13 10:25:49 +0000

    Create cert using tf

--- infra/main/webpage.tf
+++ infra/main/webpage.tf
@@ -3,11 +3,27 @@ locals {
   urls  = ["www.irdn.is", "irdn.is"]
 }
 
-data "aws_acm_certificate" "irdn" {
-  provider    = aws.us
-  domain      = "irdn.is"
-  types       = ["AMAZON_ISSUED"]
-  most_recent = true
+data "aws_route53_zone" "zone" {
+  name         = "irdn.is."
+  private_zone = false
+}
+
+module "acm_irdn" {
+  source  = "terraform-aws-modules/acm/aws"
+  version = "5.0.0"
+
+  domain_name       = "irdn.is"
+  zone_id           = data.aws_route53_zone.zone.zone_id
+  validation_method = "DNS"
+
+  subject_alternative_names = [
+    "*.irdn.is",
+  ]
+
+  wait_for_validation = true
+  providers = {
+    aws = aws.us
+  }
 }
 
 module "webpage" {
@@ -21,5 +37,5 @@ module "webpage" {
   dns_alias_enabled = true
   parent_zone_name  = "irdn.is"
 
-  acm_certificate_arn = data.aws_acm_certificate.irdn.arn
+  acm_certificate_arn = module.acm_irdn.acm_certificate_arn
 }
